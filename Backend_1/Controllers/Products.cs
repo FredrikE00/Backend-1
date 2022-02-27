1ca24de8bfe7a1295e337e72d71b19ce280436a3
FredrikE00 <fredrike0006@gmail.com>
2022-02-27 22:16:07 +0100

    backend1

---
+++ Backend_1/Controllers/Products.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private static List<Products> products = new List<Products>
+        {
+                new Products
+                {
+                    Id = 1,
+                    Name = "Jordnötter",
+                    Description = "En 350grams jordnöts påse",
+                    Price = 20,
+                    
+                },
+                new Products
+                {
+                    Id = 2,
+                    Name = "Marabou",
+                    Description = "En marabou mjölkchoklad",
+                    Price = 22,
+                   
+                },
+                new Products
+                {
+                    Id = 3,
+                    Name = "Skrivbord",
+                    Description = "Hemmagjort skrivbord i toppskick",
+                    Price = 500,
+
+                } ,
+                new Products
+                {
+                    Id = 4,
+                    Name = "Dammsugare",
+                    Description = "Dammsugare, suger upp damm",
+                    Price = 100,
+
+                }
+
+         };
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+        //Get
+        [HttpGet]
+        public async Task<ActionResult<List<Products>>> Get()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+        //Get, returnera om product inte finns
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Products>>> Get(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return BadRequest("Product not found.");
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Users>>> AddProduct(Products product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Products.ToListAsync());
+        }
+        //ändra /spara
+        [HttpPut]
+        public async Task<ActionResult<List<Products>>> UpdateProduct(Products request)
+        {
+            var Product = await _context.Products.FindAsync(request.Id);
+            if (Product == null)
+                return BadRequest("Product not found.");
+
+            Product.Name = request.Name;
+            Product.Description = request.Description;
+            Product.Price = request.Price;
+            Product.InStock = request.InStock;
+
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Products.ToListAsync());
+        }
+        //Delete en produkt
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Products>>> Delete(int id)
+        {
+            var Product = await _context.Products.FindAsync(id);
+            if (Product == null)
+                return BadRequest("Product not found.");
+
+            _context.Products.Remove(Product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Products.ToListAsync());
+        }
+    }
+    
+}
---
+++ Backend_1/Controllers/Users.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private static List<Users> users = new List<Users>
+        {
+                new Users {
+                    Id = 1,
+                    FirstName ="Fredrik",
+                    LastName="Eriksson",
+                    Email="Fredrike0006@gmail.com"
+                },
+                new Users { 
+                    Id=2,
+                    FirstName="Alfred",
+                    LastName="Modig",
+                    Email ="AlfredM@gmail.com",
+                },
+                new Users {
+                    Id=3,
+                    FirstName="Rocky",
+                    LastName="Eriksson",
+                    Email ="Rockyhund@outlook.se",
+                },
+                new Users {
+                    Id=4,
+                    FirstName="Erik",
+                    LastName="Hedberg",
+                    Email ="ErikH@gmail.com",
+                }
+        };
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        //get
+        [HttpGet]
+        public async Task<ActionResult<List<Users>>> Get()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+        //get returnera om användare inte hittas
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Users>>> Get(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return BadRequest("User not found.");
+            return Ok(user);
+        }
+        //add
+        [HttpPost]
+        public async Task<ActionResult<List<Users>>> AddUser(Users user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+        //ändra spara
+        [HttpPut]
+        public async Task<ActionResult<List<Users>>> UpdateUser(Users request)
+        {
+            var dbUser = await _context.Users.FindAsync(request.Id);
+            if (dbUser == null)
+                return BadRequest("User not found.");
+
+            dbUser.FirstName = request.FirstName;
+            dbUser.LastName = request.LastName;
+            dbUser.Email = request.Email;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+        //delete användare
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Users>>> Delete(int id)
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+            if (dbUser == null)
+                return BadRequest("User not found.");
+
+            _context.Users.Remove(dbUser);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+
+    }
+}
